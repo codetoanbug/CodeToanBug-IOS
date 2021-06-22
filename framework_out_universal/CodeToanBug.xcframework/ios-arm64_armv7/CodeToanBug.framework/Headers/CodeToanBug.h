e898e76d72c0fed525675c3601eca2982c419cef
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2021-06-22 23:05:54 +0700

    add binary

---
+++ framework_out_universal/CodeToanBug.xcframework/ios-arm64_armv7/CodeToanBug.framework/Headers/CodeToanBug.h
@@ -0,0 +1,18 @@
+//
+//  CodeToanBug.h
+//  CodeToanBug
+//
+//  Created by xuanquynhle on 2021/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeToanBug.
+FOUNDATION_EXPORT double CodeToanBugVersionNumber;
+
+//! Project version string for CodeToanBug.
+FOUNDATION_EXPORT const unsigned char CodeToanBugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeToanBug/PublicHeader.h>
+
+#import <CodeToanBug/CTBMath.h>
